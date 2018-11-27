94ea166f1789abd06234d8c979eb78da47fd803b
Oleg <o.selivancev@gmail.com>
2018-11-28 02:50:37 +0300

    providers update

--- app/providers.js
+++ app/providers.js
@@ -2,15 +2,16 @@ import axios from 'axios';
 
 let userProvider = {
     async fetch(params) {
+       //  params.text='y';
         let response = await axios.post('/api/users/select', {params}); /*let response = await axios.get*/
         return response.data;
     },
     async search(params) {
-        let response = await axios.post('http://localhost:3000/api/users/search', {params});
+        let response = await axios.post('/api/users/search', {params});
         return response.data;
     },
     async total() {
-        let response = await axios.post('http://localhost:3000/api/users/total');
+        let response = await axios.get('/api/users/total');
         return response.data;
     },
 
