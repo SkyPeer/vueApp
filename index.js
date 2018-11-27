ec605e167563afb9e70fd57df01342ae769488fa
Oleg <o.selivancev@gmail.com>
2018-11-27 08:26:55 +0300

    serverside update

--- index.js
+++ index.js
@@ -11,10 +11,30 @@ const express = require('express'),
 app.use(helmet());
 app.use(compression());
 
-app.get('/api/users/select', function(req, res, next){
-    res.sendFile('default.json', {root: __dirname});
+let jsonArray = fs.readFileSync("default.json");
+//console.log(JSON.parse(jsonArray)[1]);
+
+
+app.post('/api/users/select', bodyParser.json(), function(req, res, next){
+    //console.log('SearchbyTicketNumber req.body', req.body.params.page);
+    res.json(getRequest(req.body.params.page))
+    //res.sendFile('default.json', {root: __dirname});
     //res.header('Access-Control-Allow-Origin', '*');
+
 });
+
+function getRequest (page){
+    let array = JSON.parse(jsonArray);
+    let req = [];
+    let first = page !== 1 ? ((page-1)*10) : 0;
+    let last = page * 10;
+
+    for(let i=first; i < last; i++){
+        req.push(array[i])
+    }
+    console.log('req.length = ', req.length, 'first:', first, 'last:',last);
+    return req
+}
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/build', express.static(path.join(__dirname, 'build')));
