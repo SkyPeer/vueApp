f5ffa0c091ef4889494742e660f3a434c505ec06
Oleg <o.selivancev@gmail.com>
2018-11-28 03:30:39 +0300

    main components update

---
+++ app/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="root" class="root">
+        <main>
+            <Tabs/>
+            <router-view/>
+        </main>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+    import Tabs from "./Tabs";
+    import Footer from "./Footer";
+
+    export default {
+        name: "App",
+        components: {Tabs,Footer}
+    }
+</script>
---
+++ app/Main.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+        <h1>Hello world</h1>
+        <div>Lorem</div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "MainPage",
+    }
+</script>
