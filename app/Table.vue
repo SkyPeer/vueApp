5392c469621063a25590ffdcd7402a8cb2be50b7
Oleg <o.selivancev@gmail.com>
2018-11-28 04:33:45 +0300

    search add

--- app/Table.vue
+++ app/Table.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
+       <input placeholder="Поиск..." v-model="search"/>
+        <label>{{search}}</label>
         <table class="userTable">
             <tbody class="userTable">
             <tr class="userTable" v-for="user in users">
-                {{'id: '+ user.id + ' name: ' + user.name + ' e-meial: ' + user.email }}
+                {{'id: '+ user.id + ' name: ' + user.name + ' e-mail: ' + user.email }}
             </tr>
             </tbody>
         </table>
@@ -15,20 +17,21 @@
     import Pager from "./Pager";
     import {mapState, mapGetters} from 'vuex';
 
-
     export default {
         name: "TablePage",
         components: {Pager},
+
         data(){
             return{
-                currentPage: 1
+                currentPage: 1,
+                search :''
             }
-
         },
+
         methods: {
             changePage(page) {
                 console.log(page);
-                this.$store.dispatch('getUsers', {page: page});
+                this.$store.dispatch('getUsers', {page: page, text: this.search.length !== 0 ? this.search : null});
                 this.currentPage = page;
             }
         },
@@ -37,6 +40,16 @@
             this.$store.dispatch('getUsers', {page: query.page || 1});
             this.currentPage = query.page || 1;
             this.$store.dispatch('getTotal');
+        },
+
+        watch: {
+            search: function () {
+                this.search.length !== 0 ?
+                    this.$store.dispatch('getUsers',{page: 1, text: this.search}) : this.$store.dispatch('getUsers', {page: 1});
+
+                this.$store.dispatch('getTotal');
+            }
+
         },
 
         computed: {
